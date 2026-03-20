45445f40e7432987a697984728947c25e2117ab8
Huskies Prog <huskies5439@polych.ca>
2026-03-20 13:13:10 -0400

    ElBloups et sous system Del

---
+++ src/main/java/frc/robot/subsystems/Del.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.Percent;
+import static edu.wpi.first.units.Units.Second;
+import static edu.wpi.first.units.Units.Seconds;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Del extends SubsystemBase {
+
+  private AddressableLED del = new AddressableLED(4);
+  private AddressableLEDBuffer delBuffer = new AddressableLEDBuffer(67);
+
+  public Del() {
+
+    del.setLength(delBuffer.getLength());
+    del.setData(delBuffer);
+    del.start();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void couleur(Color color) {
+    LEDPattern solid = LEDPattern.solid(color);
+    solid.applyTo(delBuffer);
+    del.setData(delBuffer);
+  }
+
+  public void breathe(Color color) {
+    LEDPattern base = LEDPattern.solid(color);
+    LEDPattern pattern = base.breathe(Seconds.of(1.0));
+    pattern.applyTo(delBuffer);
+    del.setData(delBuffer);
+  }
+
+  public void rainbow() {
+    LEDPattern base = LEDPattern.rainbow(255, 255);
+    LEDPattern pattern = base.scrollAtRelativeSpeed(Percent.per(Second).of(25));
+    pattern.applyTo(delBuffer);
+    del.setData(delBuffer);
+  }
+
+}
